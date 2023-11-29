52480389054772f2cd52392d7c89e5e138c22d72
Gustavo Martinez <gmtz.0794@gmail.com>
2023-11-29 13:36:45 -0800

    added mutations and queries

--- Develop/server/seeds/cleanDB.js
+++ Develop/server/seeds/cleanDB.js
@@ -3,14 +3,18 @@ const db = require('../config/connection');
 
 module.exports = async (modelName, collectionName) => {
   try {
-    let modelExists = await models[modelName].db.db.listCollections({
-      name: collectionName
-    }).toArray()
+    if (modelName === 'User') {
+      await db.dropCollection('users');
+    } else {
+      let modelExists = await models[modelName].db.db.listCollections({
+        name: collectionName,
+      }).toArray();
 
-    if (modelExists.length) {
-      await db.dropCollection(collectionName);
+      if (modelExists.length) {
+        await db.dropCollection(collectionName);
+      }
     }
   } catch (err) {
     throw err;
   }
-}
+};
